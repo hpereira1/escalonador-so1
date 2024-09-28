46e986a578dee2d5c2844940c3c1bc2c38475491
hpereira1 <pereirah123@gmail.com>
2024-09-28 14:58:35 -0300

    input

--- main.cpp
+++ main.cpp
@@ -29,11 +29,36 @@ int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& pro
     for (int i = 0; i < jobs_n; i++) {
         Job j = {startTime[i], endTime[i], profit[i]};
         if (j.startTime >= j.endTime) {
-            count <<
-        }
-            jobs.push_back(j);
+            cout << "Error: startTime of job " << i << " must be less than endTime" << endl;
+            return 0;
+        }  // test if startTime is less than endTime
+        if (j.startTime < 1 || j.startTime > 1e9 || j.endTime < 1 || j.endTime > 1e9 ){
+            cout << "Error: startTime, endTime of job " << i << " must be between 1 and 1e9" << endl;
+            return 0;
+        }   // test if startTime and endTime are between 1 and 1e9
+        if (j.profit < 1 || j.profit > 1e4){
+            cout << "Error: profit of job " << i << " must be between 1 and 1e4" << endl;
+            return 0;
+        }   // test if profit is between 1 and 1e4
+        jobs.push_back(j);
+        cout << "Job " << i << " created\n" << endl;
     }   // create jobs and push them into jobs vector 
+    //print jobs
+    cout << "Jobs created: " << endl;
+    for (int i = 0; i < jobs_n; i++) {
+        cout << "Job " << i << " startTime: " << jobs[i].startTime << " endTime: " << jobs[i].endTime << " profit: " << jobs[i].profit << endl;
+    }
+    //sort jobs by endTime
+    sort(jobs.begin(), jobs.end(), [](Job a, Job b) {
+        return a.endTime < b.endTime;
+    });
+    //print sorted jobs
+    cout << "Sorted jobs" << endl;
+    for (int i = 0; i < jobs_n; i++) {
+        cout << "Job " << i << " startTime: " << jobs[i].startTime << " endTime: " << jobs[i].endTime << " profit: " << jobs[i].profit << endl;
+    }
 
+    return 0;
 }
     
 int main(){
@@ -44,10 +69,16 @@ int main(){
     vector<int> zero_er;
 
     int expected = 150;
+        
+    int result = jobScheduling(startTime, endTime, profit);
+    cout << "result: " << result << endl;
+    cout << endl;
 
+    int size_er_result = jobScheduling(startTime, endTime, size_er);
+    cout << "size_er_result: " << size_er_result << endl;
+    cout << endl;
 
-
-    
+    int zero_er_result = jobScheduling(startTime, endTime, zero_er);
 
     return 0;
 }
